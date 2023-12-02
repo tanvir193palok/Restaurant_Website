34b808d2baf00631a1d22b6e2450d905947039a4
Tanvir Ahmed Palok <118113504+tanvir193palok@users.noreply.github.com>
2023-12-03 04:17:56 +0600

    Social Component design done

--- src/components/Socials.js
+++ src/components/Socials.js
@@ -1,7 +1,25 @@
-import React from 'react';
+import React from "react";
+
+//import data
+import { socialData } from "../data";
 
 const Socials = () => {
-  return <div>Socials</div>;
+  return (
+    <div className="flex gap-x-[10px]">
+      {socialData.map((item, idx) => {
+        return (
+          <a
+            className="border border-white/20 rounded-full w-[35px] h-[35px]
+           flex justify-center items-center text-sm hover:text-accent hover:border-accent transition-all"
+            href={item.href}
+            key={idx}
+          >
+            {item.icon}
+          </a>
+        );
+      })}
+    </div>
+  );
 };
 
 export default Socials;
